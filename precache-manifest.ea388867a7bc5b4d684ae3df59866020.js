10fe3d52294e23debeaf5b48feabd535432e1e38
Rob Pemberton <hellorobertp@gmail.com>
2019-02-02 20:34:30 +0000

    Updates

---
+++ precache-manifest.ea388867a7bc5b4d684ae3df59866020.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0180c39d3929be568644",
+    "url": "/micro-dash/static/js/runtime~main.0180c39d.js"
+  },
+  {
+    "revision": "b4d67febd46764d09b5b",
+    "url": "/micro-dash/static/js/main.b4d67feb.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/micro-dash/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "b4d67febd46764d09b5b",
+    "url": "/micro-dash/static/css/main.e61e072d.chunk.css"
+  },
+  {
+    "revision": "2ed4de594bc5a036695e8573d26a522f",
+    "url": "/micro-dash/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0515834679ba0fd7da36f0df6d232876.js"
+  "/micro-dash/precache-manifest.ea388867a7bc5b4d684ae3df59866020.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/micro-dash/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
